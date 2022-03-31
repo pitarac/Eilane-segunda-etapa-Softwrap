90d1541b9f9cf5b4f526e624a54c43edb2c4d08a
pitarac <39973636+pitarac@users.noreply.github.com>
2022-03-31 00:45:33 -0300

    create usuários

---
+++ src/components/conteiner/index.js
@@ -0,0 +1,11 @@
+function Conteiner(){
+    return(
+        <div className="flex max-w-screen-xl">
+            <ul className="flex justify-evenly bg-white w-1/2 h-auto p-10">
+                <li>Usuários</li>
+                <li>Novo Usuário</li>
+            </ul>
+        </div>
+    )
+}
+export default Conteiner;
